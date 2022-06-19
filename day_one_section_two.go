d025749ebf7e84fc46a7fa8897394cf4c7121fdf
bhdrtspnr <bahadir.taspinar01@gmail.com>
2022-06-19 12:26:07 +0300

    section two

---
+++ day_one_section_two.go
@@ -0,0 +1,13 @@
+// Including the main package
+package main
+
+// Importing fmt
+import (
+	"fmt"
+)
+
+var myInt int = 100
+
+func main() { //main function can only receive arguments by os.args
+	fmt.Println("section_two")
+}
