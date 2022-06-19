eb1c986adf0ee38aa8e3fb94c754161948eeb9a4
bhdrtspnr <bahadir.taspinar01@gmail.com>
2022-06-19 11:44:59 +0300

    Day1_Session3

--- day_one.go
+++ day_one.go
@@ -4,11 +4,24 @@ package main
 // Importing fmt
 import (
 	"fmt"
+	"os"
 )
 
-// Calling main
-func main() {
+var myInt int = 100
 
-	fmt.Print("Day one")
+//yourInt := 230 //:= does declare and assign and cannot be used outside of the functions
+
+func main() { //main function can only receive arguments by os.args
+	name := os.Args[1] //here giving an argument by running go run day_one.go "testoo"
+
+	//var name string = "Bahadir"
+	var greeting = createGreet(name) //you have to have var behind any declaration
+	fmt.Printf("%s", greeting)       //printf takes %s for formatting
+
+}
+
+func createGreet(name string) string { //a function takes a string argument "name" and returns a string
+	greeting := "Hello " + name + " :) " //declaration and assignment at the same time with :=
+	return greeting
 
 }
